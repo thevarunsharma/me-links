78b915506ef54d2977f2dee4647ff4c50cfef48a
thevarunsharma <varunsharma123.del@gmail.com>
2022-02-04 21:30:52 +0530

    one webapp server instead of two

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request
+from flask import Flask, jsonify, redirect, request, render_template
 from parse import PathParser
 from store import DatabaseHandler
 from urllib.parse import urlencode
@@ -8,8 +8,14 @@ app = Flask(__name__)
 parser = PathParser()
 dbHandler = DatabaseHandler("db.dat")
 
-@app.route("/<path:arg>")
-def go(arg):
+"""
+me/ is proxied to /redirect/
+me.links/static/ is proxied to /static/
+me.links/ is proxied to /portal/
+"""
+
+@app.route("/redirect/<path:arg>")
+def me(arg):
     key, query = parser.parse(arg.strip())
     # lookup DB
     link, has_query = dbHandler.get(key)
@@ -29,6 +35,59 @@ def go(arg):
         link += query
     
     return redirect(link)
+
+@app.route("/portal/", methods=['GET'])
+def home():
+    referrer = request.args.get("referrer")
+    if referrer:
+        return render_template("index.html", has_referrer=True, referrer=referrer)
+    return render_template("index.html", has_referrer=False)
+
+@app.route("/portal/search", methods=['GET'])
+def search():
+    query = request.args.get('query')
+    # invalid search keyword
+    if not parser.is_valid(query):
+        return jsonify(
+            {
+                "status" : "INVALID",
+                "data" : []
+            }
+        )
+    matches = dbHandler.match(query.lower().strip())
+    return jsonify(
+        {
+            "status" : "FOUND" if matches else "NOT FOUND",
+            "data" : matches
+        }
+    )
+
+@app.route("/portal/check", methods=['GET'])
+def check():
+    key = request.args.get('key')
+    return jsonify(
+        {
+            "isKey" : parser.is_valid(key) and dbHandler.has_key(key)
+        }
+    )
+    
+@app.route("/portal/save", methods=['POST'])
+def save():
+    key = request.form.get("key")
+    link = request.form.get("link")
+    has_query = request.form.get("has_query")
+    if not parser.is_valid(key):
+        return "INVALID KEY", 400
+    dbHandler.set(key, link, has_query)
+    return "OK", 200
+    
+@app.route('/portal/delete', methods=['POST'])
+def delete():
+    key = request.form.get("key")
+    if parser.is_valid(key) and dbHandler.has_key(key):
+        dbHandler.pop(key)
+        return "OK", 200
+    return "Bad Request", 400
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
